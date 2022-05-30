d1a02d38f8dd8978ac947deff78b114c96121af7
Codrin Tapusa <codrin.tapusa@quartzmatrix.ro>
2022-05-30 09:59:17 +0300

    first

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+// import { CategoryItem } from '../components/category-item/category-item.component';
+import { CategoryItem } from "../category-item/category-item.component";
+import "./directory.styles.scss";
+const Directory = ({categories})=>{
+  return( <div className='categories-container'>
+  {
+    categories.map((category)=>{
+      return  <CategoryItem category={category}  key={category.id}></CategoryItem>
+    })
+  }
+</div>)
+   
+}
+
+export  default Directory;
